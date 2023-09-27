083563e7a629ad8d21effb2816e7f326628be3a5
Juan Figuera <juanantoniofiguera@gmail.com>
2023-09-27 08:49:57 -0400

    General changes

--- gestion-tiempo/src/App.js
+++ gestion-tiempo/src/App.js
@@ -1,19 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React  from "react";
 import Usuario from "./components/Usuario";
 import Admin from "./components/Admin";
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-          <Routes>
-            <Route index element={<Usuario />} />
-            <Route path="/usuario" element={<Usuario />} />
-            <Route path="/admin" element={<Admin />} />
-          </Routes>
-        </BrowserRouter>
+      <ul className="navbar">
+        <li>
+        <nav><NavLink to="/usuario">Usuario</NavLink></nav>
+        </li>
+        <li>
+        <nav><NavLink to="/admin">Admin</NavLink></nav>
+        </li>
+      </ul>
+        <Routes>
+          <Route index element={<Usuario />} />
+          <Route path="/usuario" element={<Usuario />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
